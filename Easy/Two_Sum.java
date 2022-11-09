a00b083d21904ed8259c024153fe727053f34e82
Wouter <woutermalan7909@gmail.com>
2022-11-09 14:51:33 +0200

    Two Sum Challenge Completed

--- Easy/Two_Sum.java
+++ Easy/Two_Sum.java
@@ -2,6 +2,17 @@ package Easy;
 
 public class Two_Sum {
     public int[] twoSum(int[] nums, int target) {
-        
+        int[] result = new int[2];// to store the result
+
+        for (int i = 0; i < nums.length; i++) {// loop through the array to find the first number
+            for (int j = i+1; j < nums.length; j++) {// loop through the array to find the second number
+                if(nums[i] + nums[j] == target){// if the sum of the two numbers is equal to the target
+                    result[0] = i;
+                    result[1] = j;
+                    return result;// return the result array
+                }
+            }
+        }
+        return result;
     }
 }
